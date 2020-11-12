48386174a09289bbdefe91ee5d6fb0aa82f14310
Eduardo445 <esoto-rodriguez@csumb.edu>
2020-11-11 19:30:00 -0800

    Starting the create question process

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var signupRouter = require('./routes/signup');
+var questionsRouter = require('./routes/questions');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -54,6 +55,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
+app.use('/questions', questionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
