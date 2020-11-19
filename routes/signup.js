bfb4815835c6d070148f548b4e149712f4bb258f
Yvonne Cruz <yvcruz@csumb.edu>
2020-11-19 13:00:18 -0800

    create account base

--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('signup', { title: 'Sign Up' });
+  //res.render('signup', {signUpError: false, dontMatch : false});
+  
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var user = req.body.newUsername;
+  var password = req.body.newPassword;
+  var confirmPass = req.body.confirmPassword;
+    
+  if (password == confirmPass) {
+    //access DB to see if username is taken
+      if(!results.length){      //user is not in db
+        // insert new account into the database
+          req.session.login = user;
+            if(error) {
+              req.session.destroy();
+              throw error;
+            }    
+            res.redirect('/');                
+      }else {     
+        console.log("User Already Exists!");
+        res.render('signUp.ejs', {signUpError: true, dontMatch : false});
+      }       
+  }else {
+      res.render('signUp.ejs', {signUpError : false, dontMatch : true});
+  }
+
 });
 
 module.exports = router;
