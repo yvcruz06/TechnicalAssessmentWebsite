5e5feab18e021a2c3485d0f16f6fd9f8350dc607
WoodsGeo442 <gewoods@csumb.edu>
2020-12-01 13:58:21 -0800

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -9,4 +9,30 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Log In' });
 });
 
+router.post('/', async(req, res) => {
+  
+  var user = req.body.newUsername;
+  var pass = req.body.newPassword;
+
+  
+
+});
+
+//function check db if user exists
+async function getUser(user) {
+  let list = [];
+  await User.findOne({username: user})
+  .then((result) => {
+    if (result != null) {
+      list = result;
+    }
+    else {
+      list = null;
+    }
+  }).catch((error) => {
+    console.log(error);
+  });
+  return list;
+}
+
 module.exports = router;
