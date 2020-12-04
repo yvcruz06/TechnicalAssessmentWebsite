930eeb56863b7bc0d3b3eb194e45d2f932eb8663
jacobortiz <54605823+jacobortiz@users.noreply.github.com>
2020-12-03 22:07:03 -0800

    minor

--- routes/result.js
+++ routes/result.js
@@ -7,24 +7,8 @@ const Result = require('../models/result')
 const username = 'username'
 
 router.get('/', function(req, res) {
-<<<<<<< HEAD
     res.send('working on results')
     // minor chanhes to test new branch
-=======
-
-
-
-
-
-
-
-
-
-
-    res.render('result', { Title: 'Results'})
-
-
->>>>>>> 1afd2c33db0ead3981ca8764c4ba2fe936d18d7e
 })
 
 module.exports = router
